6a2a4209a050fec4fc8b76f9c516ead81b611058
Wena <bazinavemewenaweme@gmail.com>
2026-08-06 04:28:34 +0000

    completd assignment 1  on the prime number checker

--- assignment_01_prime_checker.js
+++ assignment_01_prime_checker.js
@@ -43,3 +43,70 @@
 // =============================================================================
 
 
+
+
+const readlineSync = require('readline-sync');
+
+ 
+// this is what this function does: 
+// It checks if a number is prime
+// A prime number is > 1 and has no divisors except 1 and itself
+function isPrime(num) {
+
+    // this is what this function does: 
+    // Check if number is less than 2. If yes, it cannot be prime
+    if (num < 2) {
+        return false;
+    }
+    
+    
+    // this  function does: 
+    // 2 is the only even prime number
+    if (num === 2) {
+        return true;
+    }
+    
+    
+    // this  function if  Any other even number is not prime
+    if (num % 2 === 0) {
+        return false;
+    }
+    
+    
+    // this function checks for divisors from 3 up to the square root of the number, skipping even numbers: 
+    // If any divisor is found, the number is not prime
+    for (let i = 3; i <= Math.sqrt(num); i += 2) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+    
+    
+    // this is what this function does: 
+    // If no divisors were found, then the number is prime
+    return true;
+}
+
+ 
+// this is what this function does: 
+// It takes user input and calls isPrime to check the number
+// Then it prints whether the number is prime or not
+function main() {
+
+    // this function ask the user to enter an input : 
+ 
+    const number = readlineSync.questionInt('Enter a number: ');
+    
+    
+    // this function prints out the result of the prime check
+    if (isPrime(number)) {
+        console.log(`${number} is a prime number.`);
+    } else {
+        console.log(`${number} is NOT a prime number.`);
+    }
+}
+
+ 
+// this  starts the  program by calling the main function: 
+
+main();
